d69a2cf30c0c18365a5b4b5a8d3ec240b7601e95
mxsp <max.spolaor@gmail.com>
2014-09-09 23:00:22 -0600

    Just having fun

--- src/main/java/orrery/Orrery.java
+++ src/main/java/orrery/Orrery.java
@@ -5,6 +5,6 @@ package orrery;
  */
 public class Orrery {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Welcome to Orrery: a planetary system simulator!");
     }
 }
