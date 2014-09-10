a61bd365cb4ccc2fd04e6156769d5e30152329c0
Ryan Fox <ryan@foxrow.com>
2014-09-09 22:33:20 -0600

    mavenized

---
+++ src/main/java/orrery/Orrery.java
@@ -0,0 +1,10 @@
+package orrery;
+
+/**
+ * @author ryanfox
+ */
+public class Orrery {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/orrery/OrreryTest.java
@@ -0,0 +1,17 @@
+package orrery;
+
+import org.junit.Test;
+
+/**
+ * @author ryanfox
+ */
+public class OrreryTest {
+
+    /**
+     * Make sure main runs successfully
+     */
+    @Test
+    public void testMain() {
+        Orrery.main(new String[]{});
+    }
+}
